0ed99c3db11ad26b78bd67e8e7954777ccd19afe
83mbyte <registered.mail.mb@gmail.com>
2024-04-30 13:22:20 +0300

    added more items to DATA_TEMPLATE

--- context/HistoryContextProvider.js
+++ context/HistoryContextProvider.js
@@ -147,9 +147,24 @@ const DATA_TEMPLATE = {
         { assistant: { content: 'Accusantium neque fugiat possimus eligendi, quae adipisci animi consequatur aliquam dolorum in excepturi iste deleniti magnam quis praesentium dignissimos ullam impedit necessitatibus eos! Sed in aspernatur quisquam inventore ipsam, tempore harum ex! Culpa consectetur amet ipsum modi doloribus ratione harum laboriosam, fugit quisquam accusantium dignissimos doloremque eligendi sint, dolorem ut voluptatibus nemo itaque excepturi exercitationem vel quod accusamus soluta aliquid?', format: 'Plain text' }, user: { content: 'Libero facere ipsam quasi dolor eius doloribus' } },
 
     ],
+    1713124571101: [
+        {
+            assistant: { content: 'Provcilique aspernatur sunt incidunt, dolores veritatis aliquid dolorem dolore minima quam, ratione aut ea fugit recusandae illum distinctio, aperiam repudiandae fugiat consequatur! Laboriosam ipsum deserunt dolorum possimus, nesciunt pla am dolore corrupti hic, vitae sequi obcaecati incidunt facilis sint natus illo. Sunt, laboriosam ducimus illum accusantium excepturi sapiente tenetur obcaecati.', format: 'Plain text' }, user: { content: 'Nisi, aspernatur! Hic accusamus voluptatem, est delectus explicabo unde doloremque! Rerum iste facere assumenda beatae perspiciatis?' }
+        }
+    ],
     1714124571165: [
         {
-            assistant: { content: 'Provident hic quae ullam similique aspernatur sunt incidunt, dolores veritatis aliquid dolorem dolore minima quam, ratione aut ea fugit recusandae illum distinctio, aperiam repudiandae fugiat consequatur! Laboriosam ipsum deserunt dolorum possimus, nesciunt placeat quibusdam dolore corrupti hic, vitae sequi obcaecati incidunt facilis sint natus illo. Sunt, laboriosam ducimus illum accusantium excepturi sapiente tenetur obcaecati.', format: 'Plain text' }, user: { content: 'Nisi, aspernatur! Hic accusamus voluptatem, est delectus explicabo unde doloremque! Rerum iste facere assumenda beatae perspiciatis?' }
+            assistant: { content: 'Provident hic quae ullam similique aspernatur sunt incidunt, dolores veritatis aliquid dolorem dolore minima quam, ratione aut ea fugit recusandae illum distinctio, aperiam repudiandae fugiat consequatur! Laboriosam ipsum deserunt dolorum possimus, nesciunt placeat quibusdam dolore corrupti hic, vitae sequi obcaecati incidunt facilis sint natus illo. Sunt, laboriosam ducimus illum accusantium excepturi sapiente tenetur obcaecati.', format: 'Plain text' }, user: { content: 'Rernatur! Hic accusamus voluptatem, est delectus explicabo unde doloremque! Rerum iste facere assumenda beatae perspiciatis?' }
+        }
+    ],
+    1714124572295: [
+        {
+            assistant: { content: 'Provident hic quae ullam similique aspernatur sunt incidunt, dolores veritatis aliquid dolorem dolore minima quam, ratione aut ea fugit recusandae illum distinctio, aperiam repudiandae fugiat consequatur! Laboriosam ipsum deserunt dolorum possimus, nesciunt placeat quibusdam dolore corrupti hic, vitae sequi obcaecati incidunt facilis sint natus illo. Sunt, laboriosam ducimus illum accusantium excepturi sapiente tenetur obcaecati.', format: 'Plain text' }, user: { content: 'Csamus voluptatem, est delectus explicabo unde doloremque! Rerum iste facere assumenda beatae perspiciatis?' }
+        }
+    ],
+    1714124581398: [
+        {
+            assistant: { content: 'Provident hic quae ullam similique aspernatur sunt incidunt, dolores veritatis aliquid dolorem dolore minima quam, ratione aut ea fugit recusandae illum distinctio, aperiam repudiandae fugiat consequatur! Laboriosam ipsum deserunt dolorum possimus, nesciunt placeat quibusdam dolore corrupti hic, vitae sequi obcaecati incidunt facilis sint natus illo. Sunt, laboriosam ducimus illum accusantium excepturi sapiente tenetur obcaecati.', format: 'Plain text' }, user: { content: ' Datur! Hic accusamus voluptatem, est delectus explicabo unde doloremque! Rerum iste facere assumenda beatae perspiciatis?' }
         }
     ]
 }
