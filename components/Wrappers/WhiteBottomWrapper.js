30ac1c72f4d1d7b23dbe3cad29f362509d851585
83mbyte <registered.mail.mb@gmail.com>
2024-05-22 10:25:43 +0300

    removed Moti animation

--- components/Wrappers/WhiteBottomWrapper.js
+++ components/Wrappers/WhiteBottomWrapper.js
@@ -1,46 +1,21 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { View, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
-import { AnimatePresence, MotiView, useAnimationState } from 'moti';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const WhiteBottomWrapper = ({ keyId, route, children }) => {
     const ins = useSafeAreaInsets();
 
-    const animationState = useAnimationState({
-        from: { translateY: 1000 },
-        visible: {
-            translateY: 0,
-            transition: {
-                delay: 800,
-                type: 'timing'
-            }
-        },
-        exit: { translateY: 1000, transition: { type: 'spring' } }
-    });
-
-
-    useFocusEffect(
-        useCallback(() => {
-            animationState.transitionTo('visible');
-
-            return () => {
-                animationState.transitionTo('exit');
-            };
-        }, [route])
-    )
     return (
+
         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{ flex: 1 }} >
             <View style={[styles.container, { paddingTop: ins.top * 2 }]}>
 
-                <AnimatePresence exitBeforeEnter>
-                    <MotiView style={styles.whiteContainer} key={keyId} state={animationState} >
-                        {children}
-                    </MotiView>
-                </AnimatePresence>
+                <View style={styles.whiteContainer}>
+                    {children}
+                </View>
+
             </View>
         </KeyboardAvoidingView>
-
     );
 };
 
@@ -51,10 +26,10 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     whiteContainer: {
-        flex: 1,
+        height: '100%',
         backgroundColor: 'white',
-        borderTopLeftRadius: 15,
-        borderTopRightRadius: 15,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
         borderBottomLeftRadius: 0,
         borderBottomRightRadius: 0,
         flexDirection: 'column',
