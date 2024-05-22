a90489073b2b7442ba729b55249b36062a550df7
83mbyte <registered.mail.mb@gmail.com>
2024-05-22 12:47:41 +0300

    edited because Moti package was removed

--- components/Buttons/BackButtonHeader.js
+++ components/Buttons/BackButtonHeader.js
@@ -1,65 +1,49 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Pressable } from 'react-native';
-import { MotiView, useAnimationState } from 'moti';
+import { Pressable, View } from 'react-native';
+import Animated from 'react-native-reanimated';
+
+import animationLibrary from '../../lib/animationConfig';
 import { useFocusEffect } from '@react-navigation/native';
 
+const entering = animationLibrary.Stretch.entering;
+
 const BackButtonHeader = ({ navigation, color = '#FFF', route }) => {
+    const [show, setShow] = useState(true);
+
     const backClickHandler = () => {
 
-        backPromise().then(() => navigation.goBack());
+        // backPromise().then(() => navigation.goBack());
+        navigation.goBack()
     }
+    // const backPromise = () => {
+    //     return new Promise((resolve, reject) => {
+    //         setShow(false);
 
-    const backPromise = () => {
-
-        return new Promise((resolve, reject) => {
-            animationState.transitionTo('exit');
-            setTimeout(() => {
-                resolve();
-            }, 200)
-        })
-    }
-
-    const animationState = useAnimationState({
-        from: {
-            scale: 1,
-            opacity: 0,
-
-        },
-        to: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-                delay: 1000
-            }
-        },
-        exit: {
-            scale: 0.8,
-            opacity: 0,
-            transition: {
-                duration: 150
-            }
-        },
-    })
+    //         setTimeout(() => {
+    //             resolve();
+    //         }, 500);
+    //     })
+    // }
 
     useFocusEffect(
         useCallback(() => {
-            animationState.transitionTo('to');
-
-            return () => {
-                animationState.transitionTo('exit');
-            };
+            setShow(true);
+            return () => setShow(false);
         }, [route])
     )
 
     return (
-
-        <MotiView key={'back_btn_header'} state={animationState}>
-            <Pressable onPress={backClickHandler}>
-                <Ionicons name="return-up-back" size={28} color={color} />
-            </Pressable>
-        </MotiView>
-
+        <View style={{ backgroundColor: 'transparent', minHeight: 25 }}>
+            {
+                show &&
+                <Animated.View key={'back_btn_header'} entering={entering.delay(500)}  >
+                    <Pressable onPress={backClickHandler}>
+                        <Ionicons name="return-up-back" size={28} color={color} />
+                    </Pressable>
+                </Animated.View>
+            }
+        </View>
     )
 }
 
--- components/Buttons/ChatHeaderRightButtons.js
+++ components/Buttons/ChatHeaderRightButtons.js
@@ -2,27 +2,29 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { MotiView } from 'moti';
+import Animated from 'react-native-reanimated';
+import animationLibrary from '../../lib/animationConfig';
+
+const entering = animationLibrary.Stretch.entering;
 
 const ChatHeaderRightButtons = ({ color, onPressHistory, onPressNewChat }) => {
     return (
-        <MotiView
-            key={'chatHeaderRightButtons'}
-            from={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1000 }}
-            style={{ backgroundColor: 'transparent', flexDirection: 'row', columnGap: 10, }}
-        >
 
-            <TouchableOpacity onPress={onPressNewChat}>
-                <Ionicons name="create-outline" size={24} color={color} />
-            </TouchableOpacity>
+        <>
+            <Animated.View
+                key={'chatHeaderRightButtons'}
+                style={{ backgroundColor: 'transparent', flexDirection: 'row', columnGap: 10, }}
+                entering={entering.delay(500)}
+            >
+                <TouchableOpacity onPress={onPressNewChat}>
+                    <Ionicons name="create-outline" size={24} color={color} />
+                </TouchableOpacity>
 
-            <TouchableOpacity onPress={onPressHistory}>
-                <Ionicons name="time-outline" size={24} color={color} />
-            </TouchableOpacity>
-
-        </MotiView>
+                <TouchableOpacity onPress={onPressHistory}>
+                    <Ionicons name="time-outline" size={24} color={color} />
+                </TouchableOpacity>
+            </Animated.View>
+        </>
     );
 };
 
