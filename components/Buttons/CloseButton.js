92c1bde46711ac01f8ce9018e4539208930a57cb
83mbyte <registered.mail.mb@gmail.com>
2024-05-29 18:46:54 +0300

    styles were updated

--- components/Buttons/CloseButton.js
+++ components/Buttons/CloseButton.js
@@ -4,8 +4,8 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 const CloseButton = ({ onPressCallback }) => {
 
     return (
-        <TouchableOpacity onPress={onPressCallback}>
-            <Ionicons name="close" size={22} color="#000" />
+        <TouchableOpacity onPress={onPressCallback} style={{ backgroundColor: '#F1F1F1', borderRadius: 15, padding: 2 }}>
+            <Ionicons name="close" size={18} color='#B5B5B5' />
         </TouchableOpacity>
     );
 };
